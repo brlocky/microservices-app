304d5477a9783e930fe652e27c56a1e5c8ddbddd
Joao <brlocky@gmail.com>
2022-11-23 23:43:02 +0000

    Infrastucturer - docker-compose + .env

--- apps/gateway/src/gateway.service.ts
+++ apps/gateway/src/gateway.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GatewayService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! micro';
   }
 }
